f0e69bc9b9d9a370922eb54c3d0e44df4adb4551
benizar <benizar@gmail.com>
2022-03-09 18:31:54 +0100

    added photo in popup example

---
+++ maps/popups-with-photo/data/waypoints_2.js
@@ -0,0 +1,18 @@
+var json_waypoints_2 = {
+"type": "FeatureCollection",
+"name": "waypoints_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "popup": "<img src=\"..\/..\/data\/ftg_fieldwork_photos\/2021-04-27_15-16-30.jpg\" alt=\"Treball de camp\" style=\"width:150px;height:200px;\">" }, "geometry": { "type": "Point", "coordinates": [ 1.14789953, 41.10238893 ] } },
+{ "type": "Feature", "properties": { "popup": "<img src=\"..\/..\/data\/ftg_fieldwork_photos\/2021-04-27_15-16-53.jpg\" alt=\"Treball de camp\" style=\"width:150px;height:200px;\">" }, "geometry": { "type": "Point", "coordinates": [ 1.14820901, 41.1024105 ] } },
+{ "type": "Feature", "properties": { "popup": "<img src=\"..\/..\/data\/ftg_fieldwork_photos\/2021-04-27_15-17-26.jpg\" alt=\"Treball de camp\" style=\"width:150px;height:200px;\">" }, "geometry": { "type": "Point", "coordinates": [ 1.14836998, 41.10266959 ] } },
+{ "type": "Feature", "properties": { "popup": "<img src=\"..\/..\/data\/ftg_fieldwork_photos\/2021-04-27_15-17-47.jpg\" alt=\"Treball de camp\" style=\"width:150px;height:200px;\">" }, "geometry": { "type": "Point", "coordinates": [ 1.14836538, 41.10286004 ] } },
+{ "type": "Feature", "properties": { "popup": "<img src=\"..\/..\/data\/ftg_fieldwork_photos\/2021-04-27_15-18-02.jpg\" alt=\"Treball de camp\" style=\"width:200px;height:150px;\">" }, "geometry": { "type": "Point", "coordinates": [ 1.14833016, 41.10301004 ] } },
+{ "type": "Feature", "properties": { "popup": "<img src=\"..\/..\/data\/ftg_fieldwork_photos\/2021-04-27_15-18-45.jpg\" alt=\"Treball de camp\" style=\"width:150px;height:200px;\">" }, "geometry": { "type": "Point", "coordinates": [ 1.147747, 41.10301357 ] } },
+{ "type": "Feature", "properties": { "popup": "<img src=\"..\/..\/data\/ftg_fieldwork_photos\/2021-04-27_15-19-05.jpg\" alt=\"Treball de camp\" style=\"width:150px;height:200px;\">" }, "geometry": { "type": "Point", "coordinates": [ 1.14748927, 41.10303951 ] } },
+{ "type": "Feature", "properties": { "popup": "<img src=\"..\/..\/data\/ftg_fieldwork_photos\/2021-04-27_15-19-160.jpg\" alt=\"Treball de camp\" style=\"width:150px;height:200px;\">" }, "geometry": { "type": "Point", "coordinates": [ 1.14749561, 41.10299947 ] } },
+{ "type": "Feature", "properties": { "popup": "<img src=\"..\/..\/data\/ftg_fieldwork_photos\/2021-04-27_15-19-45.jpg\" alt=\"Treball de camp\" style=\"width:150px;height:200px;\">" }, "geometry": { "type": "Point", "coordinates": [ 1.14745294, 41.10263596 ] } },
+{ "type": "Feature", "properties": { "popup": "<img src=\"..\/..\/data\/ftg_fieldwork_photos\/2021-04-27_15-24-12.jpg\" alt=\"Treball de camp\" style=\"width:150px;height:200px;\">" }, "geometry": { "type": "Point", "coordinates": [ 1.14727561, 41.10249246 ] } },
+{ "type": "Feature", "properties": { "popup": "<img src=\"..\/..\/data\/ftg_fieldwork_photos\/2021-04-27_15-24-40.jpg\" alt=\"Treball de camp\" style=\"width:150px;height:200px;\">" }, "geometry": { "type": "Point", "coordinates": [ 1.14775977, 41.10242821 ] } }
+]
+}
